f58568847922f95996f6287b41e56eca55fdfe19
Marcelo e Erika <erika.campos.design@gmail.com>
2018-02-20 22:01:59 -0300

    Add association RecipeType wiht Recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216000206) do
+ActiveRecord::Schema.define(version: 20180221005009) do
 
   create_table "cuisines", force: :cascade do |t|
     t.string "name"
@@ -18,9 +18,14 @@ ActiveRecord::Schema.define(version: 20180216000206) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "recipe_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "recipes", force: :cascade do |t|
     t.string "title"
-    t.string "recipe_type"
     t.string "difficulty"
     t.integer "cook_time"
     t.datetime "created_at", null: false
