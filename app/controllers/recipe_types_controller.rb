9888a7cbf49625178fd9fcd95bee18ec75689046
Marcelo e Erika <erika.campos.design@gmail.com>
2018-02-22 19:51:49 -0300

    Visitor view recipes by type

--- app/controllers/recipe_types_controller.rb
+++ app/controllers/recipe_types_controller.rb
@@ -1,8 +1,15 @@
 class RecipeTypesController < ApplicationController
-
   def show
     @recipe_type = RecipeType.find(params[:id])
-    @recipes = Recipe.all
+
+    if @recipe_type.recipes.empty?
+      flash[:notice] = msg
+    end
   end
 
+  private
+
+  def msg
+    'Nenhuma receita encontrada para este tipo de receitas'
+  end
 end
