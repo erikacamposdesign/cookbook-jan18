1838b9cc886ee533fe0d7d5fcc7474b24cafbc73
Marcelo e Erika <erika.campos.design@gmail.com>
2018-02-26 20:37:04 -0300

    User register recipe_types

--- app/controllers/recipe_types_controller.rb
+++ app/controllers/recipe_types_controller.rb
@@ -1,4 +1,20 @@
 class RecipeTypesController < ApplicationController
+
+  def create
+    @recipe_type = RecipeType.new(create_params)
+
+    if @recipe_type.save
+      redirect_to recipe_type_path(@recipe_type)
+    else
+      flash[:notice] = 'Você deve informar o nome do tipo de receita'
+      render :new
+    end
+  end
+
+  def new
+    @recipe_type = RecipeType.new
+  end
+
   def show
     @recipe_type = RecipeType.find(params[:id])
 
@@ -12,4 +28,9 @@ class RecipeTypesController < ApplicationController
   def msg
     'Nenhuma receita encontrada para este tipo de receitas'
   end
+
+  def create_params
+    params.require(:recipe_type).permit(:name)
+  end
+
 end
